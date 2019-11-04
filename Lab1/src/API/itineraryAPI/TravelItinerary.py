d1ea4cb3033180d5af4b13da5f3fa65ebe1c1948
Robert Szarvas <robisz98@gmail.com>
2019-11-04 13:49:13 +0200

    added some implementation calling into api

--- Lab1/src/API/itineraryAPI/TravelItinerary.py
+++ Lab1/src/API/itineraryAPI/TravelItinerary.py
@@ -1,9 +1,35 @@
+import json
+import requests
+
+from Lab1.src import settings
+
+
 class Location:
 
     def __init__(self, name, latitude, longitude):
         self.__name = name
         self.__longitude = longitude
         self.__latitude = latitude
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def longitude(self):
+        return self.__longitude
+
+    @property
+    def latitude(self):
+        return self.__latitude
+
+    def __eq__(self, o: object) -> bool:
+        if o is not Location: return False
+        return self.latitude == o.latitude and self.longitude == o.longitude
+
+    def __ne__(self, o: object) -> bool:
+        return not self == o
+
 
 class Instruction:
     def __init__(self, start_date_time, end_date_time, start_location, end_location, distance = 0):
@@ -14,7 +40,7 @@ class Instruction:
         self.__distance = distance
 
     def is_transition(self):
-        pass
+        return self.__distance == 0 and self.__start_location != self.__end_location
 
 
 class TravelItinerary:
@@ -24,9 +50,45 @@ class TravelItinerary:
         self.__start_location = start_location
         self.__end_location = end_location or start_location
         self.__visits = []
+        self.__to_visit = []
+        self.__agents = [{
+            'name' : 'travelPlanner',
+            "shifts": [
+                {
+                    "startTime": start_date_time,
+                    "startLocation": {
+                        "latitude": self.__start_location.latitude,
+                        "longitude": self.__start_location.longitude
+                    },
+                    "endTime": end_date_time,
+                    "endLocation": {
+                        "latitude": self.__end_location.latitude,
+                        "longitude": self.__end_location.longitude
+                    }
+                }
+            ]
+        }]
 
-    def add_visit(self, location, date_of_visit, staying_time, priority, opening_time='00:00:00', closing_time='00:00:00'):
-        pass
+    def add_visit(self, location, date_of_visit, staying_time, priority, opening_time='00:00:00', closing_time='23:59:59'):
+        visit = {
+            "name": location.name,
+            "OpeningTime": date_of_visit + 'T' + opening_time,
+            "ClosingTime": date_of_visit + 'T' + closing_time,
+            "DwellTime": staying_time,
+            "Priority": int(priority),
+            "Location": {
+                "Latitude": location.latitude,
+                "Longitude": location.longitude
+            }
+        }
+        self.__to_visit.append(visit)
 
     def compute_route(self):
-        pass
+        requestJSON = {
+            'agents' : self.__agents,
+            'itineraryItems' : self.__to_visit
+        }
+        requestJSON = json.dumps(requestJSON)
+        headers = {'content-type': 'application/json'}
+        response = requests.post(settings.OPTIMIZE_ITINERARY + settings.API_KEY, data=requestJSON, headers=headers)
+        print(response.text)
---
+++ Lab1/src/test.py
@@ -0,0 +1,14 @@
+from Lab1.src.API.itineraryAPI.TravelItinerary import *
+
+l = [Location("loc1", 46.779792, 23.620796),
+     Location("loc2", 46.766435, 23.589105),
+     Location("loc3", 46.775976, 23.603794)]
+
+ti = TravelItinerary("2019-11-09T08:00:00", "2019-11-09T22:00:00",
+                Location("start", 46.774775, 23.621636), Location("start", 46.774775, 23.621636))
+
+ti.add_visit(l[0], "2019-11-09", "01:00:32.6770000", 1)
+ti.add_visit(l[1], "2019-11-09", "02:00:32.6770000", 2)
+ti.add_visit(l[2], "2019-11-09", "00:30:32.6770000", 3)
+
+ti.compute_route()
